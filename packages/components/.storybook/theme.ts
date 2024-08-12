26a1aea578bc1198ac7a621f66f92d5942e04afa
wangkai.1997 <wangkka1@163.com>
2024-08-12 10:48:13 +0800

    fix: fix site logo url and tailwindcss content build

--- packages/components/.storybook/theme.ts
+++ packages/components/.storybook/theme.ts
@@ -3,6 +3,6 @@ import { create } from "@storybook/theming/create";
 export default create({
   base: "light",
   brandTitle: "dev-kit",
-  brandImage: "/logo.png",
+  brandImage: "logo.png",
   brandTarget: "_self",
 });
