e9f5384272615b15744f25980e160d927d37b588
wangkai.1997 <wangkka1@163.com>
2024-08-29 17:22:31 +0800

    fix(components): fix components export error

--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -2,5 +2,6 @@ import "./index.css";
 
 export * from "./react-draggable-modal";
 export * from "./react-error-boundary";
+export * from "./react-pdf-viewer";
 export * from "./react-progress";
 export * from "./react-spin";
