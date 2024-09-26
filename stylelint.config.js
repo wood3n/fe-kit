b02a943054f719db4bd819523844443eeec48b88
wangkai.1997 <wangkka1@163.com>
2024-09-26 19:28:38 +0800

    fix: change lint config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,9 +1,11 @@
 /** @type {import('stylelint').Config} */
 export default {
   extends: "stylelint-config-standard",
-  plugins: ["stylelint-prettier"],
+  plugins: ["stylelint-order", "stylelint-prettier"],
   rules: {
     "prettier/prettier": true,
+    "order/order": ["custom-properties", "declarations"],
+    "order/properties-order": ["width", "height"],
     "block-no-empty": true,
     "color-hex-length": "short",
     "at-rule-no-unknown": [
